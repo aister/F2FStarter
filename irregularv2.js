922db54531600a9b05df943762a701b73791b737
aister <hellodecasino@gmail.com>
2024-09-01 15:46:00 +0700

    Create irregularv2.js

---
+++ irregularv2.js
@@ -0,0 +1,38 @@
+const mode1 = "V0 ⇒ V2";
+const mode2 = "V2 ⇒ V0";
+  let words = {
+    "Từ chưa nhớ": `
+      begin, began
+      break, broke
+      build, built
+      bring, brought
+      draw, drew
+      buy, bought
+      be, was/were
+      sell, sold
+      lose, lost
+      say, said
+      take, took
+      sleep, slept
+      think, thought
+      stand, stood
+      teach, taught
+      wear, wore
+      catch, caught
+      choose, chose
+      cost, cost
+      drive, drove
+      fall, fell
+      feel, felt
+      hear, heard
+      hurt, hurt
+      lend, lent
+      let, let
+      pay, paid
+      send, sent
+      spend, spent
+      stand, stood
+      tell, told
+      win, won
+    `
+  };
