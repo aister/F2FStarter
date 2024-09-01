1d3e198347238fba2ab5030469a40bfc3a2e81ec
aister <hellodecasino@gmail.com>
2024-09-01 15:27:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,130 @@
+
+  let words = {
+    "Quốc gia": `
+      Ý, Italy - Italian
+      Bra-zin, Brazil - Brazilian
+      Nga, Russia - Russian
+      Mỹ, the USA - American
+      Đức, Germany - German
+      Ai Cập, Egypt - Egyptian
+      Úc, Australia - Australian
+      Mexico, Mexico - Mexican
+      Thổ Nhĩ Kỳ, Turkey - Turkish
+      Anh, the UK - British
+      Trung Quốc, China - Chinese
+      Tây Ban Nha, Spain - Spanish
+      Việt Nam, Vietnam - Vietnamese
+    `,
+    "Đồ vật": `
+      giỏ, bag
+      từ điển, dictionary
+      táo, apple
+      bút bi, pen
+      bút chì, pencil
+      sách, book
+      tập, notebook
+      máy nghe nhạc, iPod
+      điện thoại, mobile
+      dù, umbrella
+      em bé, baby
+      bé trai, boy
+      bé gái, girl
+      người đàn ông, man
+      người phụ nữ, woman
+      nhật ký, diary
+      ghế, chair
+      bàn, table
+      máy tính, computer
+      máy chụp hình, camera
+      đồng hồ đeo tay, watch
+      bánh kẹp, sandwich
+    `,
+    "Công việc": `
+      quản lý, manager
+      bác sĩ, doctor
+      diễn viên nam, actor
+      diễn viên nữ, actress
+      nhân viên bán hàng, shop assistant
+      giáo viên, teacher
+      bồi bàn nam, waiter
+      bồi bàn nữ, waitress
+      tài xế taxi, taxi driver
+      nhạc sĩ, musician
+      cảnh sát, police officer
+      sinh viên, student
+    `,
+    "Thông tin": `
+      tên, first name
+      họ, surname
+      có gia đình, married
+      độc thân, single
+      quốc tịch, nationality
+      địa chỉ, address
+      số điện thoại, mobile number
+      địa chỉ email, email address
+    `,
+    "Chữ số": `
+      0, zero
+      1, one
+      2, two
+      3, three
+      4, four
+      5, five
+      6, six
+      7, seven
+      8, eight
+      9, nine
+      10, ten
+      11, eleven
+      12, twelve
+      13, thirteen
+      14, fourteen
+      15, fifteen
+      16, sixteen
+      17, seventeen
+      18, eighteen
+      19, nineteen
+      20, twenty
+      30, thirty
+      40, forty
+      50, fifty
+      60, sixty
+      70, seventy
+      80, eighty
+      90, ninety
+      100, one hundred
+    `,
+    "Nhân xưng": `
+      tôi, I
+      chúng tôi, we
+      bạn, you
+      anh ấy, he
+      cô ấy, she
+      nó, it
+      chúng nó, they
+      của tôi, my
+      của chúng tôi, our
+      của bạn, your
+      của anh ấy, his
+      của cô ấy, her
+      của nó, its
+      của chúng nó, their
+    `,
+    "Tính từ": `
+      tốt, good
+      xấu, bad
+      nóng, hot
+      lạnh, cold
+      lớn, big
+      nhỏ, small
+      mới, new
+      cũ, cold
+      mắc, expensive
+      rẻ, cheap
+      đẹp, beautiful
+      xấu, ugly
+      thân thiện, friendly
+      không thân thiện, unfriendly
+      tuyệt vời, nice
+    `
+  };
