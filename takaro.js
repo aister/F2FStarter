0382a429befd0411747ca40644875f86c13f0070
aister <hellodecasino@gmail.com>
2026-03-12 16:55:01 +1300

    Create takaro.js

---
+++ takaro.js
@@ -0,0 +1,37 @@
+const mode1 = "Image ⇒ Words";
+const mode2 = "Words ⇒ Image";
+  let words = {
+    "Feelings": `
+			<img src="img/amazed.webp">, mīharo - amazed
+			<img src="img/angry.webp">, pukuriri - angry
+			<img src="img/bossy.webp">, tonotono - bossy
+			<img src="img/caring.webp">, manaaki - caring
+			<img src="img/confused.webp">, pōhēhē - confused
+			<img src="img/crying.webp">, tangitangi - crying
+			<img src="img/curious.webp">, māhirahira - curious
+			<img src="img/disappointed.webp">, ketekete - disappointed
+			<img src="img/embarassed.webp">, whakamā - embarassed
+			<img src="img/excited.webp">, manahau - excited
+			<img src="img/frustrated.webp">, whakararu - frustrated
+			<img src="img/grumpy.webp">, kiriweti - grumpy
+			<img src="img/happy.webp">, harikoa - happy
+			<img src="img/hungry.webp">, hiakai - hungry
+			<img src="img/impatient.webp">, kaikaha - impatient
+			<img src="img/laughter.webp">, katakata - laughter
+			<img src="img/listening.webp">, whakarongo - listening
+			<img src="img/love.jpg">, aroha - love
+			<img src="img/nervous.webp">, taiatea - nervous
+			<img src="img/proud.webp">, whakahī - proud
+			<img src="img/sad.webp">, pōuri - sad
+			<img src="img/scared.webp">, mataku - scared
+			<img src="img/shy.png">, hūiki - shy
+			<img src="img/sick.webp">, māuiui - sick
+			<img src="img/silly.webp">, heahea - silly
+			<img src="img/sleepy.png">, hiamoe - sleepy
+			<img src="img/smiling.webp">, menemene - smiling
+			<img src="img/surprised.webp">, ohorere - surprised
+			<img src="img/talk.webp">, kōrero - talk
+			<img src="img/thirsty.webp">, hiainu - thirsty
+			<img src="img/tired.png">, ngenge - tired
+    `,
+  };
